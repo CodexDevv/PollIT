81cb96b9cc650f36364bd547c78a10c3639f3e22
CodexDEV <stefanga2020@gmail.com>
2023-11-18 19:07:19 +0200

    Added JWT Authorization

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { hash, compare } = require("bcryptjs");
 const { verify } = require("jsonwebtoken");
+const bodyParser = require("body-parser");
 
 const db = require("./database");
 const userCollection = require("./model/Users");
@@ -9,15 +12,25 @@ const userCollection = require("./model/Users");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendRefreshToken,
+  sendAccessToken,
+} = require("./tokens.js");
+
+const checkAuth = require("./checkAuth.js");
+
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
@@ -25,11 +38,9 @@ app.post("/register", async (req, res) => {
   try {
     const user = await userCollection.findOne({ email: email });
     if (user) throw new Error("User already exists");
-    await userCollection.create({ email, password });
 
-    // const hasedPassword = await bcrypt.hash(password, 10);
-
-    //database.push({ email, password: hashedPassword });
+    const hashedPassword = await hash(password, 10);
+    await userCollection.create({ email: email, password: hashedPassword });
     res.status(201).send({ message: "User created" });
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -40,12 +51,54 @@ app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await userCollection.findOne({ email: email });
+    if (!user) throw new Error("User does not exist");
 
-    if (user.password !== password) throw new Error("Wrong password");
-    res.status(200).send({ message: "Login successful" });
+    const isValid = await compare(password, user.password);
+
+    if (!isValid) throw new Error("Wrong password");
+
+    const accesstoken = createAccessToken(user.id);
+    const refreshtoken = createRefreshToken(user.id);
+
+    user.refreshtoken = refreshtoken;
+
+    sendRefreshToken(res, refreshtoken);
+    sendAccessToken(res, req, accesstoken);
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("refreshtoken", { path: "/refresh_token" });
+  return res.send({
+    message: "Logged out",
+  });
+});
+
+app.post("/refresh_token", (req, res) => {
+  const token = req.cookies.refreshtoken;
+  if (!token) return res.send({ accesstoken: "" });
+
+  let payload = null;
+
+  try {
+    payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+  } catch (err) {
+    return res.send({ accesstoken: "" });
+  }
+
+  const user = userCollection.findOne({ id: payload.userId });
+  if (!user) return res.send({ accesstoken: "" });
+
+  if (user.refreshtoken !== token) return res.send({ accesstoken: "" });
+
+  const accesstoken = createAccessToken(user.id);
+  const refreshtoken = createRefreshToken(user.id);
+
+  user.refreshtoken = refreshtoken;
+  sendRefreshToken(res, refreshtoken);
+  return res.send({ accesstoken });
 });
 
 app.get("/", (req, res) => {
